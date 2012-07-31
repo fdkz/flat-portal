ee1f31c05d98a4a7100a321abc9f78cdc69d2c64
Elmo Trolla <fdfdkz@gmail.com>
2012-08-01 02:33:35 +0300

    Add layout of the new FlatPortalDriver application - a flat-portal GUI/wiimote handler that sends IR dot coordinates over UDP to the flat-portal renderer process.

---
+++ FlatPortalDriver/FlatPortalDriver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlatPortalDriver
+//
+//  Created by Elmo Trolla on 21/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ FlatPortalDriver/FlatPortalDriver/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  FlatPortalDriver
+//
+//  Created by Elmo Trolla on 21/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+	
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
